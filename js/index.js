93a6525ecfa810c6dc18b64220ee0e883cefc2e6
Fernando Delgado Bravo <68391460+fernando-delgado-2003@users.noreply.github.com>
2021-08-08 13:23:02 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,51 +1,52 @@
-const screenForm =(btn,wrapBtn)=>{
-    let login = document.getElementsByClassName("login")[0],
-        register = document.getElementsByClassName("register")[0];
-        btn != "facebook" ? wrapBtn.style.display="none": "";
-   switch (btn) {
-        case 'login':
-                register.classList.remove("active")
-                login.classList.add("active");
-            break;
-        case 'register':
-            login.classList.remove("active");
-            register.classList.add("active")
-            break;
-        case 'facebook':
-            const messageFacebook = document.querySelector(".facebook span");
-            messageFacebook.classList.add("active");
-            setTimeout(()=>{
-                messageFacebook.classList.remove("active")
-            }, 1500)
-            break;
-        
-        
-   } 
-}
+const screenForm = (btn, place, wrapBtn) => {
+  let login = document.getElementsByClassName("login")[0],
+    register = document.getElementsByClassName("register")[0];
+  btn != "facebook" ? (wrapBtn[1].style.display = "none") : "",
+  classCss = place == "nav-form" ? "active" : "active-move";
+  switch (btn) {
+    case "login":
+      register.classList.remove(classCss);
+      login.classList.add(classCss);
+      break;
+    case "register":
+      login.classList.remove(classCss);
+      register.classList.add(classCss);
+      break;
+    case "facebook":
+      const messageFacebook = document.querySelector(".facebook span");
+      messageFacebook.classList.add("active");
+      setTimeout(() => {
+        messageFacebook.classList.remove("active");
+      }, 1500);
+      break;
+  }
+};
 
 const app = () => {
+  const btnsNavForm = document.querySelectorAll(".btn"),
+    wrapBtn = document.querySelectorAll(".options");
+console.log(btnsNavForm)
+  btnsNavForm.forEach((elem, i, list) => {
+    elem.addEventListener("click", (e) => {
+      if (document.getElementById("nav-form-form")) {
+        const navForm = document.querySelector(".nav-form");
+        if (
+          !navForm.classList.contains("active") &&
+          e.target.dataset.name != "facebook"
+        ) {
+          navForm.classList.add("active");
+          list[5].style.opacity = "1";
+          list[5].addEventListener("click", () => {
+            list[5].style.opacity = "0";
+            wrapBtn[1].style.display = "flex";
+            navForm.classList.remove("active");
+          });
+        }
+        console.log(e.target.dataset)
+        screenForm(e.target.dataset.name, e.target.dataset.place, wrapBtn);
+      }
+    });
+  });
+};
 
-    const btnsNavForm = document.querySelectorAll(".nav-form button"),
-        wrapBtn = document.querySelector(".options");
-
-    btnsNavForm.forEach((elem, i, list) => {
-        elem.addEventListener("click", (e) => {
-            const navForm = document.querySelector(".nav-form");
-            if (!navForm.classList.contains("active") && e.target.dataset.name != "facebook") {
-                navForm.classList.add("active")
-                list[3].style.opacity = "1";
-                list[3].addEventListener("click", () => {
-                    list[3].style.opacity = "0";
-                    wrapBtn.style.display="flex";
-                    navForm.classList.remove("active");
-                })
-
-
-            }
-
-            screenForm(e.target.dataset.name, wrapBtn)
-        })
-    })
-}
-
-app()
+app();
